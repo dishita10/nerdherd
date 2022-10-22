1f299d6add5b41d98ce1f31553490e410f193c80
dishita10 <dishuhazarika@gmail.com>
2022-10-22 12:58:55 +0530

    first commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Rank.css' ;
+
+const Rank=()=>{
+    return(
+        <div className='white f3' >
+            <div className='textcol'>
+            {'thank you for your service.'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
